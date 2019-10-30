186e3b144e484fdfa7116558f13279ba8afe437f
Mustafa Kursun <mustafa.kursun@lab.futuready.com>
2019-10-30 17:37:47 +0700

    bycrypt implementation

--- server.js
+++ server.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const bycrypt = require('bcrypt-nodejs');
+const cors = require('cors');
 
 const app = express();
 
 app.use(bodyParser.json());
+app.use(cors());
 
 const database = {
   users: [
@@ -23,6 +26,13 @@ const database = {
       entries: 0,
       joined: new Date(),
     }
+  ],
+  login: [
+    {
+      id: '987',
+      hash: '',
+      email: 'john@gmail.com'
+    }
   ]
 }
 
@@ -35,6 +45,12 @@ app.get('/users', (req, res) => {
 });
 
 app.post('/signin', (req, res) => {
+  bycrypt.compare('apples', '$2a$10$qmlFntP.SmBUvflqbTmcHem1bgifXWzAQy6O0ueeIBykXG.qgajji', (err, res) => {
+    console.log('first guess', res);
+  });
+  bycrypt.compare('veggies', '$2a$10$qmlFntP.SmBUvflqbTmcHem1bgifXWzAQy6O0ueeIBykXG.qgajji', (err, res) => {
+    console.log('second guess', res);
+  })
   if (req.body.email === database.users[0].email) {
     res.json('Success!');
   } else {
@@ -44,6 +60,9 @@ app.post('/signin', (req, res) => {
 
 app.post('/register', (req, res) => {
   const { email, name, password } = req.body;
+  bycrypt.hash(password, null, null, (err, hash) => {
+    console.log(hash);
+  })
 
   database.users.push({
     id: '125',
@@ -57,8 +76,37 @@ app.post('/register', (req, res) => {
   res.json(database.users[database.users.length - 1]);
 });
 
-app.listen(process.env.PORT || 3000, () => {
-  console.log(`Listening on port ${process.env.PORT || 3000}...`);
+app.get('/profile/:id', (req, res) => {
+  const { id } = req.params;
+  let found = false;
+  database.users.forEach(user => {
+    if(user.id === id) {
+      found = true;
+      res.json(user);
+    }
+  })
+  if(!found) {
+    res.status(400).json('user not found');
+  }
+});
+
+app.post('/image', (req, res) => {
+  const { id } = req.body;
+  let found = false;
+  database.users.forEach(user => {
+    if(user.id === id) {
+      found = true;
+      user.entries++;
+      res.json(user.entries);
+    }
+  })
+  if(!found) {
+    res.status(400).json('user not found');
+  }
+});
+
+app.listen(process.env.PORT || 3030, () => {
+  console.log(`Listening on port ${process.env.PORT || 3030}...`);
 });
 
 /* FUNDAMENTAL STEPS/ENDPOINTS
